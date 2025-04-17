d229038b6007c90dfe4e24ff0931de48c0fba596
The Berufegoru <giftmoobi@gmail.com>
2025-04-18 00:25:58 +0200

    refactor(transformers): remove unused transformer classes and related exports

--- src/transformers/index.ts
+++
@@ -1,7 +0,0 @@
-export { AdminTransformer } from './admin.transformer';
-
-export { CandidateTransformer } from './candidate.transformer';
-
-export { EmployerTransformer } from './employer.transformer';
-
-export { UserTransformer } from './user.transformer';
