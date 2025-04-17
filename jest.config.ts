852be9f81ab37edf880dfc670de33d3154bfee01
The Berufegoru <giftmoobi@gmail.com>
2025-04-18 00:11:29 +0200

    feat(jest): add jest-stare reporter for enhanced test reporting and coverage links feat(project): update asset configuration to include EJS templates and improve output structure fix(tsconfig): include specific worker files in TypeScript compilation

--- jest.config.ts
+++ jest.config.ts
@@ -12,4 +12,18 @@ export default {
     '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
     '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
   ],
+  reporters: [
+    'default',
+    [
+      'jest-stare',
+      {
+        resultDir: 'jest-stare',
+        reportTitle: 'jest-stare!',
+        additionalResultsProcessors: ['jest-junit'],
+        coverageLink: '../../coverage/lcov-report/index.html',
+        jestStareConfigJson: 'jest-stare.json',
+        jestGlobalConfigJson: 'globalStuff.json',
+      },
+    ],
+  ],
 };
